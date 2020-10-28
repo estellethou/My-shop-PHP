8059f8e05cfab6811790a419fbfcb5782092163e
Estelle <estelle.thou@gmail.com>
2020-10-28 17:24:33 +0100

    verify if email already existed

--- admin.php
+++ admin.php
@@ -102,13 +102,18 @@ class User {
 
     public function insertUserIntoDb(){
         $bdd = connectToDb();
-        $response = $bdd->prepare("INSERT INTO users(username, password, email, admin, created_at) 
+        $query = $bdd->prepare("SELECT email FROM users WHERE email ='" . $_POST['email'] . "';");
+        $query->execute();
+        $arrayUsers = $query->fetchAll();
+        if ($arrayUsers == null) {
+            $response = $bdd->prepare("INSERT INTO users(username, password, email, admin, created_at) 
                                   VALUES (?, ?, ?, ?, NOW());");
-        $response->bindParam(1, $this->username, PDO::PARAM_STR);
-        $response->bindParam(3, $this->email, PDO::PARAM_STR);
-        $response->bindParam(2, $this->password, PDO::PARAM_STR);
-        $response->bindParam(4, $this->admin, PDO::PARAM_INT);
-        $response->execute();
+            $response->bindParam(1, $this->username, PDO::PARAM_STR);
+            $response->bindParam(3, $this->email, PDO::PARAM_STR);
+            $response->bindParam(2, $this->password, PDO::PARAM_STR);
+            $response->bindParam(4, $this->admin, PDO::PARAM_INT);
+            $response->execute();
+        }
     }
 } 
 
